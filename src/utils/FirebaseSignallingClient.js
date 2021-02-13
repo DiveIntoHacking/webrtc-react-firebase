afa06fb02aa0edc740bbb57b4b89dd7ebc9ac922
Atsushi Ishida <gipcompany@gmail.com>
2021-02-13 22:03:45 +0900

    exclude firebase keys

--- src/utils/FirebaseSignallingClient.js
+++ src/utils/FirebaseSignallingClient.js
@@ -3,14 +3,24 @@ import 'firebase/database';
 
 export default class FirebaseSignallingClient {
   constructor() {
+    const {
+      REACT_APP_FIREBASE_API_KEY,
+      REACT_APP_FIREBASE_AUTH_DOMAIN,
+      REACT_APP_FIREBASE_DATABASE_URL,
+      REACT_APP_FIREBASE_PROJECT_ID,
+      REACT_APP_FIREBASE_STORAGE_BUCKET,
+      REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+      REACT_APP_FIREBASE_APP_ID,
+    } = process.env;
+
     const firebaseConfig = {
-      apiKey: 'AIzaSyBywOR167-miFzoIkRRqF-ZGXucjY8KBtg',
-      authDomain: 'webrtc-react-firebase.firebaseapp.com',
-      databaseURL: 'https://webrtc-react-firebase-default-rtdb.firebaseio.com',
-      projectId: 'webrtc-react-firebase',
-      storageBucket: 'webrtc-react-firebase.appspot.com',
-      messagingSenderId: '434102252074',
-      appId: '1:434102252074:web:bafb5e334d3da3933d8953',
+      apiKey: REACT_APP_FIREBASE_API_KEY,
+      authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+      databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
+      projectId: REACT_APP_FIREBASE_PROJECT_ID,
+      storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+      appId: REACT_APP_FIREBASE_APP_ID,
     };
     if (firebase.apps.length === 0) firebase.initializeApp(firebaseConfig);
     this.database = firebase.database();
